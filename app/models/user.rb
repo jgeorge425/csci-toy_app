08b9693a296f42a1baee6e6f26b5c4a646dd80a2
Johnny George <jpgeorge@email.uscupstate.edu>
2017-01-30 20:56:13 +0000

    Fixed final bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   has_many :microposts
-  validates :content, length: {maximum: 25},  presence: true
-  validates :content, length: {maximum: 50},  presence: true
+ # validates :content, length: {maximum: 25},  presence: true
+ # validates :content, length: {maximum: 50},  presence: true
 end
